31bae9e695d164265456335fc543973bf8acf9af
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2019-03-24 13:45:52 +0100

    Add layout buttons

--- src/plugins/system/id4me/tmpl/login.php
+++ src/plugins/system/id4me/tmpl/login.php
@@ -1,10 +1,34 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.ID4Me
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+defined('_JEXEC') or die;
 
-echo 'hallo';
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Factory;
+
+HTMLHelper::_('stylesheet', 'plg_system_id4me/id4me.css', ['relative' => true]);
+HTMLHelper::_('script', 'plg_system_id4me/id4me.min.js', ['relative' => true]);
+
+$js = "
+	(function(document, Joomla)
+	{
+	  document.addEventListener('DOMContentLoaded', function()
+	  {
+		if (Joomla.ID4Me)
+		{
+			Joomla.ID4Me('.login > form, form#login-form', {
+				buttonimage: " . json_encode(HTMLHelper::_('image', 'plg_system_id4me/id4me-login-button.svg', '', null, true, 1)) . "
+			});
+		}
+	  });
+	})(document, Joomla);
+";
+
+Factory::getDocument()->addScriptDeclaration($js);
+
