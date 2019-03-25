61d03c0b3ca4ec147971eeb904709bae46a599ad
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2019-03-25 11:43:51 +0100

    Implement next steps

--- src/plugins/system/id4me/id4me.php
+++ src/plugins/system/id4me/id4me.php
@@ -35,9 +35,11 @@ class PlgSystemId4me extends CMSPlugin
 
 	protected $autoloadLanguage = true;
 
+	protected $type = 'native';
+
 	public function onBeforeRender()
 	{
-		if ($this->app->isClient('site') || ($this->app->isClient('administrator') && Factory::getUser()->guest))
+		if ($this->app->isClient('site') /*|| ($this->app->isClient('administrator') && Factory::getUser()->guest)*/)
 		{
 			// Load JS
 			echo $this->loadLayout('login');
@@ -60,18 +62,23 @@ class PlgSystemId4me extends CMSPlugin
 		// Validate identifier:
 		$issuer = $this->getIssuerbyIdentifier($identifier);
 
-		// We can't do anythng when there is no issuer
-		if (!$issuer)
-		{
-			return false;
-		}
-
 		$issuerUrl = Uri::getInstance($issuer);
 		$issuerUrl->setScheme('https');
 
 		$issuerConfiguration = $this->getOpenIdConfiguration($issuerUrl->toString());
 
-		$authorizationUrl = $this->getAuthorizationUrl($issuerConfiguration, $identifier);
+		$registrationEndpoint = (string) $issuerConfiguration->get('registration_endpoint');
+		$registrationResult = $this->registerService($registrationEndpoint);
+
+		$client_id = $registrationResult->get('client_id');
+		$client_secret = $registrationResult->get('client_secret');
+		$state = UserHelper::genRandomPassword(100);
+
+		$this->app->setUserState('id4me.client_id', $client_id);
+		$this->app->setUserState('id4me.client_secret', $client_secret);
+		$this->app->setUserState('id4me.state', $state);
+
+		$authorizationUrl = $this->getAuthorizationUrl($issuerConfiguration, $identifier, $client_id, $state);
 
 		if (!$authorizationUrl)
 		{
@@ -94,24 +101,111 @@ class PlgSystemId4me extends CMSPlugin
 		// Validate identifier:
 		$issuer = $this->getIssuerbyIdentifier($identifier);
 
-		// We can't do anythng when there is no issuer
-		if (!$issuer)
-		{
-			return false;
-		}
-
 		$issuerUrl = Uri::getInstance($issuer);
 		$issuerUrl->setScheme('https');
 
 		$issuerConfiguration = $this->getOpenIdConfiguration($issuerUrl->toString());
+
+		$registrationEndpoint = (string) $issuerConfiguration->get('registration_endpoint');
+		$registrationResult = $this->registerService($registrationEndpoint);
+
+		$client_id = $this->app->getUserState('id4me.client_id');
+		$client_secret = $this->app->getUserState('id4me.client_secret');
+
 		$tokenEndpoint = (string) $issuerConfiguration->get('token_endpoint');
 
-		$this->getAuthTokens($code, $tokenEndpoint);
+		$tokens = $this->getAuthTokens($code, $tokenEndpoint, $client_id, $client_secret);
+
+		$bearerToken = $this->validateAuthTokens($tokens);
+
+		$userinfoEndpoint = (string) $issuerConfiguration->get('userinfo_endpoint');
+
+		$claims = $this->getClaims($userinfoEndpoint, $bearerToken);
+
+		echo '<pre>';print_r($claims);exit('foo');
 
 	}
 
+	protected function dieHard()
+	{
+		throw new Exception(Text::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'));
+	}
 
-	protected function getAuthTokens($code, $tokenEndpoint)
+	protected function getClaims($userinfoEndpoint, $token)
+	{
+		$headers = [
+			'Accept' => 'application/jwt',
+			'Authorization' => 'Bearer ' . $token
+		];
+
+		$claimResult = HttpFactory::getHttp()->get($userinfoEndpoint, $headers);
+
+		if (empty($claimResult->body) || $claimResult->code != '200')
+		{
+			$this->dieHard();
+		}
+
+		$claims = new Registry($claimResult->body);
+
+		$this->validateClaims($claims);
+
+		return $this->loadRealClaims($claims);
+	}
+
+	protected function loadRealClaims($claimResult)
+	{
+		$claimNames = $claimResult->get('_claim_names');
+		$claimRessources = (array) $claimResult->get('_claim_sources');
+
+		$endpoints = [];
+
+		$user = new stdClass;
+
+		if (!isset($claimRessources[$claimNames->given_name])|| !isset($claimRessources[$claimNames->email]))
+		{
+			$this->dieHard();
+		}
+
+		$user->name = $this->loadClaimByKey($claimRessources[$claimNames->given_name]->endpoint, $claimRessources[$claimNames->given_name]->access_token, 'given_name');
+		$user->email = $this->loadClaimByKey($claimRessources[$claimNames->given_name]->endpoint, $claimRessources[$claimNames->given_name]->access_token, 'endpoint');
+
+		print_r($user);exit;
+
+		return $user;
+	}
+
+	protected function loadClaimByKey($endpoint, $token, $name)
+	{
+		static $ressources = [];
+
+		if (isset($ressources[$endpoint][$token]))
+		{
+			return $ressources[$endpoint][$token]->get($name);
+		}
+
+		$headers = [
+			'Accept' => 'application/jwt',
+			'Authorization' => 'Bearer ' . $token
+		];
+
+		$claimResult = HttpFactory::getHttp()->get($endpoint, $headers);
+
+		if (empty($claimResult->body) || $claimResult->code != '200')
+		{
+			$this->dieHard();
+		}
+
+		$ressources[$endpoint][$token] = new Registry($claimResult->body);
+
+		return $ressources[$endpoint][$token]->get($name);
+	}
+
+	protected function validateCLaims($claimResult)
+	{
+		return true;
+	}
+
+	protected function getAuthTokens($code, $tokenEndpoint, $client_id, $client_secret)
 	{
 		$authTokenRequest = http_build_query([
 			'grant_type' => 'authorization_code',
@@ -119,14 +213,30 @@ class PlgSystemId4me extends CMSPlugin
 			'redirect_uri' => $this->getValidateUrl(),
 		]);
 
-		$registrationResult = HttpFactory::getHttp()->post($tokenEndpoint, $authTokenRequest, ['Content-Type' => 'application/x-www-form-urlencoded']);
+		$headers = [
+			'Content-Type' => 'application/x-www-form-urlencoded',
+			'Authorization' => 'Basic ' . base64_encode($client_id . ':' . $client_secret)
+		];
+
+		$registrationResult = HttpFactory::getHttp()->post($tokenEndpoint, $authTokenRequest, $headers);
 
 		if (empty($registrationResult->body) || $registrationResult->code != '200')
 		{
-			return false;
+			$this->dieHard();
 		}
 
 		return new Registry($registrationResult->body);
+	}
+
+	protected function validateAuthTokens($tokens)
+	{
+		// @TODO token validation
+		if ($tokens->get('access_token'))
+		{
+			return $tokens->get('access_token');
+		}
+
+		$this->dieHard();
 	}
 
 	/**
@@ -150,13 +260,8 @@ class PlgSystemId4me extends CMSPlugin
 		return $result;
 	}
 
-	protected function getAuthorizationUrl($issuerConfiguration, $identifier)
+	protected function getAuthorizationUrl($issuerConfiguration, $identifier, $client_id, $state)
 	{
-		$type = 'native';
-
-		$registrationEndpoint = (string) $issuerConfiguration->get('registration_endpoint');
-		$registrationResult = $this->registerService($registrationEndpoint, $type);
-
 		$claimsSupported = $issuerConfiguration->get('claims_supported');
 		$claims = $this->getUserInfoClaims($claimsSupported);
 
@@ -166,11 +271,11 @@ class PlgSystemId4me extends CMSPlugin
 		$authorizationUrl->setVar('claims', urlencode($claims));
 		$authorizationUrl->setVar('scope', 'openid');
 		$authorizationUrl->setVar('response_type', 'code');
-		$authorizationUrl->setVar('client_id', $registrationResult->get('client_id'));
-		$authorizationUrl->setVar('redirect_uri', urlencode($this->getValidateUrl($type)));
+		$authorizationUrl->setVar('client_id', $client_id);
+		$authorizationUrl->setVar('redirect_uri', urlencode($this->getValidateUrl()));
 		$authorizationUrl->setVar('login_hint', $identifier);
-		$authorizationUrl->setVar('state', UserHelper::genRandomPassword(100));
-		$authorizationUrl->setScheme($type === 'web' ? 'https' : 'http');
+		$authorizationUrl->setVar('state', $state);
+		$authorizationUrl->setScheme($this->type === 'web' ? 'https' : 'http');
 
 		return $authorizationUrl->toString();
 	}
@@ -199,15 +304,14 @@ class PlgSystemId4me extends CMSPlugin
 	/**
 	 *
 	 *
-	 * @param type $type
 	 *
 	 * @return string  The validateion URL
 	 */
-	protected function getValidateUrl($type)
+	protected function getValidateUrl()
 	{
 		$redirectUrl = Uri::getInstance();
 		$redirectUrl->setQuery(self::$validateUrl);
-		$redirectUrl->setScheme($type === 'web' ? 'https' : 'http');
+		$redirectUrl->setScheme($this->type === 'web' ? 'https' : 'http');
 
 		return $redirectUrl->toString();
 	}
@@ -215,23 +319,22 @@ class PlgSystemId4me extends CMSPlugin
 	/**
 	 *
 	 * @param type $registrationEndpoint
-	 * @param type $type
 	 *
 	 * @return boolean|Registry
 	 */
-	protected function registerService($registrationEndpoint, $type = 'web')
+	protected function registerService($registrationEndpoint)
 	{
 		$registrationDataJSON = json_encode(array(
 			'client_name' => $this->app->get('sitename'),
-			'application_type' => $type,
-			'redirect_uris' => [$this->getValidateUrl($type)],
+			'application_type' => $this->type,
+			'redirect_uris' => [$this->getValidateUrl()],
 		));
 
 		$registrationResult = HttpFactory::getHttp()->post($registrationEndpoint, $registrationDataJSON, ['Content-Type' => 'application/json']);
 
-		if (empty($registrationResult->body) || $registrationResult->code != '200')
+		if (empty($registrationResult->body) || !in_array($registrationResult->code, ['200', '201']))
 		{
-			return false;
+			$this->dieHard();
 		}
 
 		return new Registry($registrationResult->body);
@@ -247,6 +350,7 @@ class PlgSystemId4me extends CMSPlugin
 	private function getIssuerbyHostname($hostname)
 	{
 		$hostname = '_openid.' . $hostname;
+
 		$records = dns_get_record($hostname, DNS_TXT);
 
 		if (empty($records) || !is_array($records))
@@ -271,7 +375,7 @@ class PlgSystemId4me extends CMSPlugin
 			}
 		}
 
-		return false;
+		$this->dieHard();
 	}
 
 	/**
@@ -288,7 +392,6 @@ class PlgSystemId4me extends CMSPlugin
 		$totalCountOfHostparts = count($hostparts);
 
 		$i = 0;
-		$result = false;
 
 		do
 		{
@@ -305,7 +408,7 @@ class PlgSystemId4me extends CMSPlugin
 		}
 		while ($i <= $totalCountOfHostparts);
 
-		return false;
+		$this->dieHard();
 	}
 
 	/**
@@ -323,7 +426,7 @@ class PlgSystemId4me extends CMSPlugin
 
 		if (empty($openIdConfiguration->body) || $openIdConfiguration->code != '200')
 		{
-			return false;
+			$this->dieHard();
 		}
 
 		return new Registry($openIdConfiguration->body);
