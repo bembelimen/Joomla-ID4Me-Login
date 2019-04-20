eede6c3c693938f9cf257c7e0d7847349f485e7f
zero-24 <zero-24@users.noreply.github.com>
2019-04-20 13:20:31 +0200

    add missing css, add mising language string, split language files, also load in the backend

--- src/plugins/system/id4me/id4me.php
+++ src/plugins/system/id4me/id4me.php
@@ -37,12 +37,13 @@ class PlgSystemId4me extends CMSPlugin
 
 	public function onBeforeRender()
 	{
-		if ($this->app->isClient('site') /*|| ($this->app->isClient('administrator') && Factory::getUser()->guest)*/)
+		if ($this->app->isClient('site') || ($this->app->isClient('administrator') && Factory::getUser()->guest))
 		{
-			// Load JS
-			echo $this->loadLayout('login');
-
+			// Load the language string
 			Text::script('PLG_SYSTEM_ID4ME_IDENTIFIER_LABEL');
+
+			// Load the layout with the JaveScript Code
+			echo $this->loadLayout('login');
 		}
 	}
 
